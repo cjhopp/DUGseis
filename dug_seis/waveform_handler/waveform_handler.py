51f2a6f9a17f27d9ab7b998ae3746170d4a50640
cjhopp <chopp@lbl.gov>
2023-08-28 11:13:10 -0600

    filling gaps between files

--- dug_seis/waveform_handler/waveform_handler.py
+++ dug_seis/waveform_handler/waveform_handler.py
@@ -597,7 +597,7 @@ class WaveformHandler:
         print(st.__str__(extended=True))
         st.trim(obspy.UTCDateTime(start_time), obspy.UTCDateTime(end_time))
         print(st.__str__(extended=True))
-        st.select(id=id).write('test_gap_fill.ms', format="MSEED")
+        st.select(id=i).write('test_gap_fill.ms', format="MSEED")
         st.merge()
         print(st.__str__(extended=True))
         if return_trace:
